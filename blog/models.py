4d4aa2dee37d35270771003520c08f0aaf15a0d8
zekeriyeyevmuxtar <zekeriyeyevmuxtar547@gmail.com>
2023-05-23 13:24:32 +0400

    biraz kod yazdlm
    Merhaba Serhat

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+class Categories(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Mehsul kataqorisi')
 
 class Mehsullar(models.Model):
     name = models.CharField(max_length=100, verbose_name='Mehsul adi')
